62ac67c573653e9a790fc8a0a4277aa5021fb7d8
Oleg Beriashvili <Oleg.Beriashvili@jetbrains.com>
2025-07-16 11:12:11 +0000

    vite config  allowedHosts: true

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['*.preview.matter.labs.jb.gg'],
     port: 3000,
     host: true,
     allowedHosts: [
-      'preview-dec5-ffa3-725f-d225.preview.matter.labs.jb.gg'
+      'allowedHosts: true'
     ]
   }
 })
